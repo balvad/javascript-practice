dc66b112d506aaea53027d36088f6e148caa380f
Balaji Vadlamudi <bava72@gmail.com>
2020-09-23 13:10:00 -0700

    commit to take-home

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let score=12;
+const myName='djskdjksjd';
+myName='jskjdkjsdk';
+score++;
+let isAble=true;
+
+let myObj = {
+    myName: 'Balaji'
+}
+
+myObj.myName='balaji-2';
+
+//Single line comment
+/*
+multi line comment 1
+multi line comment 2
+*/
